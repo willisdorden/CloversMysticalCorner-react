98e21c8d26f91445517af9f73c472f80aa6bb1ef
Admin <admin@Willis-MacBook-Pro.local>
2017-08-21 20:59:50 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-008a28aa65ca4137a4d8.js","410c85bcb571789b16b4add699002bc1"],["/app-251fbbcb7fa57c84ea03.js","58bc6fbe5e4000749a1fac0126b93f26"],["/app-27571a650779271fa07a.js","98e0c0f0f4b65b2d68758995363f7bf9"],["/commons-342eaaf2c50a1d38a175.js","698acd36beba4da22689ff0a1122c28c"],["/commons-d9333dc6b8f5289fea78.js","60ec7f30fbf5b4da104c62611959aa60"],["/commons-e3ee9b1c9073847c03f9.js","c0f3026f0f83d0f1edda520faedc0d02"],["/commons-f11d53c679121e43cf4f.js","7e767cc300eac459e65eab416cfb1aea"],["/index.html","20acd4408a6e0c16811a04abd266b45f"],["/manifest.json","5a1863cc9e8dc72ccf9dfcdba5ca3ec4"],["/offline-plugin-app-shell-fallback/index.html","5f2314190da0bce7f2d2279efb64cab2"]];
+var precacheConfig = [["/app-008a28aa65ca4137a4d8.js","410c85bcb571789b16b4add699002bc1"],["/app-251fbbcb7fa57c84ea03.js","58bc6fbe5e4000749a1fac0126b93f26"],["/app-27571a650779271fa07a.js","98e0c0f0f4b65b2d68758995363f7bf9"],["/commons-342eaaf2c50a1d38a175.js","698acd36beba4da22689ff0a1122c28c"],["/commons-73d96a6f61ce036e9059.js","4f5a83b7a5ab96d1e6194c90e881cb9c"],["/commons-d9333dc6b8f5289fea78.js","60ec7f30fbf5b4da104c62611959aa60"],["/commons-e3ee9b1c9073847c03f9.js","c0f3026f0f83d0f1edda520faedc0d02"],["/commons-f11d53c679121e43cf4f.js","7e767cc300eac459e65eab416cfb1aea"],["/index.html","cc8937ccaa45385067702ca884e5b043"],["/manifest.json","5a1863cc9e8dc72ccf9dfcdba5ca3ec4"],["/offline-plugin-app-shell-fallback/index.html","ea11a14e948a4ead3b6fb1cad2f28240"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
